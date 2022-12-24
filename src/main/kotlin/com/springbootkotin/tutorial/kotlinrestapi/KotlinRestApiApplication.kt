c3cead73901841361499d3aedb4a001c943495e7
Diwas Kumar Arya <diwasarya25@gmail.com>
2022-12-24 16:23:18 +0530

    Initial commit

---
+++ src/main/kotlin/com/springbootkotin/tutorial/kotlinrestapi/KotlinRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.springbootkotin.tutorial.kotlinrestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/springbootkotin/tutorial/kotlinrestapi/KotlinRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.springbootkotin.tutorial.kotlinrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
